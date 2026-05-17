c8f9deb57c36b9bf4980276684455eca64a1efba
Antidamage <adeline@skull.co.nz>
2026-05-17 18:00:39 +1200

    Add cache control headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=3600, stale-if-error=86400",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
